2123eedb2e4a75cea496e184f6190977bcbc4ffd
mikehwagz <mike@selfaware.studio>
2021-03-16 00:48:11 -0400

    update example

--- examples/index.js
+++ examples/index.js
@@ -5,25 +5,26 @@ import '../styles.css'
 
 function App() {
   const [count, increment] = useReducer((c) => c + 1, 0)
+
   return (
     <div>
       <Marqy>
-        <h1 className="py-025 mr-025">Default configuration ●</h1>
+        <p className="text-32 py-025 mr-025">Default configuration ●</p>
       </Marqy>
       <Marqy className="invert">
-        <h1 className="py-025 mr-025">
+        <p className="text-32 py-025 mr-025">
           Synchronized speeds even if content length is different.
-        </h1>
+        </p>
       </Marqy>
       <Marqy direction="right">
-        <h1 className="py-025 mr-025">You can change the direction…</h1>
+        <p className="text-32 py-025 mr-025">You can change the direction…</p>
       </Marqy>
       <Marqy className="invert" direction="right" speed="0.25">
-        <h1 className="py-025 mr-025">And customize the speed!</h1>
+        <p className="text-32 py-025 mr-025">And customize the speed!</p>
       </Marqy>
       <Marqy direction="right" speed="0.4">
         <div className="flex items-center py-05">
-          <h1 className="mx-05">Render whatever you want:</h1>
+          <p className="text-32 mx-05">Render whatever you want:</p>
           <img
             width="100"
             src="https://www.fillmurray.com/g/200/200"
@@ -33,7 +34,7 @@ function App() {
       </Marqy>
       <Marqy className="invert" speed="0.7" pauseOnHover>
         <div className="flex items-center py-025">
-          <h1>Pause on hover for interactivity:</h1>
+          <p className="text-20">Pause on hover for interactivity:</p>
           <button className="mx-05" onClick={increment}>
             Count: {count}
           </button>
